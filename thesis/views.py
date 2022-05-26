4cc641dc9bfaec42c325d8a67df00d37d340b798
Khalil Adib <Khaliladib11@gmail.com>
2022-05-26 22:04:33 +0100

    configure static files

--- thesis/views.py
+++ thesis/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def theses(request):
+    theses = []
+    context = {
+        'theses': theses
+    }
+    return render(request, 'thesis/theses.html', context)
